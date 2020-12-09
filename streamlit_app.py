2755b1e3c908a8881ea284000cf2630291dc7ab1
Guido Rainuzzo <hi@guido.nyc>
2020-12-09 17:20:23 -0500

    more dirs

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,14 @@ time.sleep(10)
 
 import streamlit as st
 import os
+import os.path
 
 st.title("your secret")
 
 st.text(os.getcwd())
-st.text(os.expanduser('~'))
+st.text(os.path.expanduser('~'))
 
-with open("../secrets/secrets.toml") as f:
+with open(".streamlit/secrets.toml") as f:
   st.text(f.read())
 
 st.json(dict(os.environ))
