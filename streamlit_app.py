310db07e8d21caf0af1d8c3c8ba5d261316e7aa1
Guido Rainuzzo <hi@guido.nyc>
2021-02-09 13:51:09 -0500

    version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,7 @@
 import streamlit as st
+import sys
 
-st.title("main app 16")
+st.title("python version:")
+st.text(sys.version_info)
 
-value = st.slider("Pick a number", 0, 10, 3)
 
-st.write(value)
-
-input = st.text_input("Tell me something", "Cantami o Diva")
-
-with open("temp_file.txt", "a") as f:
-  f.write(f"{input}\n")
-  
-with open("temp_file.txt", "r") as f:
-  st.write(f.read())
-
-st.write("Streamlit is fabulous")
-
-st.write("Hello Corey, this is the demo!")
-st.balloons()
-
-print("this is a log line")
-
-uploaded_file = st.file_uploader("Choose a Jpg file", type="jpg")
-if uploaded_file is not None:
-    st.write(uploaded_file)
